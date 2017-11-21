6856a805da7879830e8f4701898c60917ef2f20b
jberry93 <jeffreyberry93@gmail.com>
2017-11-21 14:47:05 -0800

    refactor(tests): deleted all tests except for index.text to try out only Enzyme

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import renderer from 'react-test-renderer'
 import App from '../pages/index'
 
 describe('index', () => {
-  it('renders w/out crashing: Enzyme', () => {
+  it('renders w/out crashing', () => {
     shallow(<App/>)
-  })
-  it('renders w/out crashing: Snapshot', () => {
-    const component = renderer.create(<App/>)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
   })
 })
